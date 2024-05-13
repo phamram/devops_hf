7f40bf3b8a74a582af1af04075ece358f8ca503b
phamram <pham.tientung@gmail.com>
2024-05-13 12:25:45 +0200

    string app & test done.

---
+++ StringApp/Methods.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringApp
+{
+    public class Methods
+    {
+        public static string RemoveFirstChar(string str)
+        {
+            string[] words = str.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (!string.IsNullOrEmpty(words[i]))
+                {
+                    words[i] = words[i].Substring(1);
+                }
+            }
+            return string.Join(" ", words);
+        }
+
+        public static string RemoveLastChar(string str)
+        {
+            string[] words = str.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (!string.IsNullOrEmpty(words[i]))
+                {
+                    words[i] = words[i].Substring(0, words[i].Length - 1);
+                }
+            }
+            return string.Join(" ", words);
+        }
+    }
+}
---
+++ StringApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "Hello IT maintainance class !";
+            Console.WriteLine("Original string: " + input);
+            Console.WriteLine("After..");
+            Console.WriteLine("First character of each word removed: " + Methods.RemoveFirstChar(input));
+            Console.WriteLine("Last character of each word removed: " + Methods.RemoveLastChar(input));
+
+        }
+    }
+}
