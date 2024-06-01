8d40a94395115522a9f8bdbcfb73d3789ba2e639
phamram <pham.tientung@gmail.com>
2024-06-01 09:10:20 +0200

    change 2

---
+++ StringAppTests/StringTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using NUnit.Framework;
+using StringApp;
+
+namespace StringApp.StringAppTests
+{
+    public class StringTests
+    {
+        [Test]
+        public void RemoveFirstChar_Test()
+        {
+            // Arrange
+            string input = "Hello IT maintainance class !";
+            string expectedOutput = "ello T aintainance lass";
+
+            // Act
+            string result = Methods.RemoveFirstChar(input);
+
+            // Assert
+            Assert.AreEqual(expectedOutput, result);
+        }
+    }
+}
